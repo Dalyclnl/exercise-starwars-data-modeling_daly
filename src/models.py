b6442ef871b02b7919022e8661cc9ef454806777
Dalyclnl <dalyarmijoa@yahoo.com>
2022-11-19 12:35:34 +0000

    2do commit

--- src/models.py
+++ src/models.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+import enum  
 
 
 ##class User(Base):
@@ -27,52 +28,67 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Media_enum(enum.enum):
+    imagen = "imagen",
+    video = "video"
+
+
 class User(Base):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     user_name = Column(String(250),primary_key=True)
     name = Column(String(250))
-    firstname = Column(String(250))
-    lastname = Column(String(250))
+    pasword = Column(String(250))
     email = Column(String(250))
-
-
-class Favorites (Base):
-    __tablename__ = 'favorites'
-    id = Column(Integer, primary_key=True)
-    followers_id = Column(Integer, ForeignKey('followers.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
-    user_id = Column(Integer, ForeignKey('user.id'))
-    media_id = Column(Integer, ForeignKey('media.id'))
-    favorites = relationship("user")
 
 
 class Followers (Base):
     __tablename__ = 'followers'
     id = Column(Integer, primary_key=True)
     name = Column(String(250))
-    last_name = Column(String(250))
-    details = Column(String(250))
+    user_from = Column(String(250))
+    user_to  = Column(String(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    media_id = Column(Integer, ForeignKey('media.id'))
     followers = relationship("user")
 
 
 class Post (Base):
     __tablename__ = 'post'
     id = Column(Integer, primary_key=True)
-    post = Column(String(250))
+    user_id =Column(String(250)),ForeignKey('user.id')
+    date = Column(String(250))
+    text = Column(String(250))
+    media = Column(String(250))
+    followers_id = Column(Integer, ForeignKey('followers.id'))
     post = relationship("user")
+    post = relationship("media")
  
+class Comment (Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, ForeignKey('media.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment = Column(String(250))
+    comment = relationship("user")
+    comment = relationship("media")
 
 
 class Media (Base):
     __tablename__ = 'media'
     id = Column(Integer, primary_key=True)
+    type = Column(String(250))
+    url = Column(EnumString(250))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
     media = Column(String(250))
     media = relationship("user")
-    media = relationship("favorites")
+    media = relationship("post")
 
     def to_dict(self):
         return {}
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
